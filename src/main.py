c5484f6886e23261dfd3b0fcef5e3f21f8d7eb9f
Stan Kurek <33377239+stankurek@users.noreply.github.com>
2025-01-29 18:24:30 +0100

    add get_sampling_rate_from_json

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import os
-from utils import load_eeg_arff, eeg_dataframe_to_json
+from utils import load_eeg_arff, eeg_dataframe_to_json, get_sampling_rate_from_json
 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_dir, "../data/EEG_Eye_State.arff")
 
-df = load_eeg_arff(file_path)
+df = load_eeg_arff(file_path, False)
 json_data = eeg_dataframe_to_json(df)
-print(json_data)
+sampling_rate = get_sampling_rate_from_json(json_data, 117)
+print(sampling_rate)
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 from scipy.io import arff
 import pandas
+import json
 
 def load_eeg_arff(file_path, verbose=True):
   """
@@ -35,10 +36,21 @@ def eeg_dataframe_to_json(df):
             print("No data to converts")
             return None
 
-        eeg_json = df.to_json(orient="records", indent=4)
+        eeg_json_string = df.to_json(orient="records", indent=4)
+        eeg_json = json.loads(eeg_json_string)
 
         return eeg_json
     except Exception as e:
         print(f"An error occured during converting to JSON: {e}")
         return None
-  
+
+
+def get_sampling_rate_from_json(eeg_json, duration_in_seconds):
+    try:
+        samples = len(eeg_json)
+        sampling_rate = samples / duration_in_seconds
+
+        return round(sampling_rate)
+    except Exception as e:
+        print(f"An error occured during getting sampling rate: {e}")
+        return None
