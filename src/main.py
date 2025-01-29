57e8ca97b5d72a9cfd159fbcbc74e4a00e5b2fe6
Stan Kurek <33377239+stankurek@users.noreply.github.com>
2025-01-29 18:41:43 +0100

    remove unnecessary conversion to json

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 import os
-from utils import load_eeg_arff, eeg_dataframe_to_json, get_sampling_rate_from_json
+from utils import load_eeg_arff, get_sampling_rate_from_dataframe
 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_dir, "../data/EEG_Eye_State.arff")
 
 df = load_eeg_arff(file_path, False)
-json_data = eeg_dataframe_to_json(df)
-sampling_rate = get_sampling_rate_from_json(json_data, 117)
+sampling_rate = get_sampling_rate_from_dataframe(df, 117)
 print(sampling_rate)
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,5 @@
 from scipy.io import arff
 import pandas
-import json
 
 def load_eeg_arff(file_path, verbose=True):
   """
@@ -26,31 +25,15 @@ def load_eeg_arff(file_path, verbose=True):
     print(f"An error occurred during loading data: {e}")
     return None
 
-def eeg_dataframe_to_json(df):
-    """
-    Converts EEG data to JSON.
-    Returnsstring or None.
-    """
+def get_sampling_rate_from_dataframe(df, duration_in_seconds):
     try:
         if df is None or df.empty:
-            print("No data to converts")
-            return None
+            raise ValueError("DataFrame is empty or None.")
 
-        eeg_json_string = df.to_json(orient="records", indent=4)
-        eeg_json = json.loads(eeg_json_string)
-
-        return eeg_json
-    except Exception as e:
-        print(f"An error occured during converting to JSON: {e}")
-        return None
-
-
-def get_sampling_rate_from_json(eeg_json, duration_in_seconds):
-    try:
-        samples = len(eeg_json)
+        samples = len(df)
         sampling_rate = samples / duration_in_seconds
 
         return round(sampling_rate)
     except Exception as e:
-        print(f"An error occured during getting sampling rate: {e}")
+        print(f"An error occurred while calculating sampling rate: {e}")
         return None
