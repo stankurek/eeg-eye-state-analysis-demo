a77219227a62dc43d2f288503c495491ff050c4b
Stan Kurek <33377239+stankurek@users.noreply.github.com>
2025-01-29 17:18:45 +0100

    add eeg_dataframe_to_json

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import os
-from utils import load_eeg_arff
+from utils import load_eeg_arff, eeg_dataframe_to_json
 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_dir, "../data/EEG_Eye_State.arff")
 
 df = load_eeg_arff(file_path)
-print(df)
-
+json_data = eeg_dataframe_to_json(df)
+print(json_data)
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,10 @@ from scipy.io import arff
 import pandas
 
 def load_eeg_arff(file_path, verbose=True):
+  """
+  Loads EEG data in arff format.
+  Returns DataFrame or None.
+  """
   try:
     data, metadata = arff.loadarff(file_path)
     df = pandas.DataFrame(data)
@@ -20,3 +24,21 @@ def load_eeg_arff(file_path, verbose=True):
   except Exception as e:
     print(f"An error occurred during loading data: {e}")
     return None
+
+def eeg_dataframe_to_json(df):
+    """
+    Converts EEG data to JSON.
+    Returnsstring or None.
+    """
+    try:
+        if df is None or df.empty:
+            print("No data to converts")
+            return None
+
+        eeg_json = df.to_json(orient="records", indent=4)
+
+        return eeg_json
+    except Exception as e:
+        print(f"An error occured during converting to JSON: {e}")
+        return None
+  
